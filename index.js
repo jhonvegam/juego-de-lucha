762edae750b13a23511043927cbe647ecc7bd007
jhonvegam <jhongerardovega@gmail.com>
2024-03-19 22:21:52 -0500

    Se ha creado el servidor y se envían nombres y posición del enemigo pero ahora quiero agregar enemigos reales y no puestos a mano

---
+++ index.js
@@ -0,0 +1,89 @@
+//libreria para conectar el servidor con la aplicación
+const express = require("express")
+//libreria para que no me genere errores de conexión
+const cors = require("cors")
+
+const app = express()
+
+//deshabilitar todos los posibles errores por cors
+app.use(cors())
+//habilitar recibir peticiones post en formato json
+app.use(express.json())
+
+const jugadores = []
+
+class Jugador{
+    constructor(id){
+        this.id = id
+    }
+
+    asignarMokepon(mokepon)
+    {
+        this.mokepon = mokepon
+    }
+
+    actualizarPosicion(x,y)
+    {
+        this.x = x
+        this.y = y
+    }
+}
+
+class Mokepon {
+    constructor(nombre)
+    {
+        this.nombre = nombre
+    }
+}
+
+app.get("/unirse", (req, res) =>{
+    const id = `${Math.random()}`
+
+    const jugador = new Jugador(id)
+
+    jugadores.push(jugador)
+
+    res.setHeader("Access-Control-Allow-Origin", "*")
+
+    res.send(id)
+})
+
+app.post("/mokepon/:jugadorId", (req, res) => {//se ponen 2 puntos porque es la forma de definir que ahí va una variable (jugadorId es una variable que será pasada por parametro)
+    const jugadorId = req.params.jugadorId || "" //Se accede a la variable que se envió por medio de la URL
+    const nombre = req.body.mokepon || "" //accede al body del json que se envió 
+    const mokepon = new Mokepon(nombre)
+    const jugadorIndex= jugadores.findIndex((jugador)=> jugadorId === jugador.id)
+
+    if(jugadorIndex>=0)
+    {
+        jugadores[jugadorIndex].asignarMokepon(mokepon)
+    }
+
+    console.log(jugadores)
+    console.log(jugadorId)
+    res.end()
+}) 
+
+app.post("/mokepon/:jugadorId/posicion", (req, res) => {
+    const jugadorId = req.params.jugadorId || "" //Se accede a la variable que se envió por medio de la URL
+    const x = req.body.x || 0
+    const y = req.body.y || 0
+
+    const jugadorIndex= jugadores.findIndex((jugador)=> jugadorId === jugador.id)
+
+    if(jugadorIndex>=0)
+    {
+        jugadores[jugadorIndex].actualizarPosicion(x,y)
+    }
+
+    const enemigos = jugadores.filter((jugador) => jugadorId !== jugador.id)
+
+    res.send({
+        enemigos
+    })
+})  
+
+
+app.listen(8080, () => {
+    console.log("Servidor funcionando")
+})
--- js/mokepon.js
+++ js/mokepon.js
@@ -22,6 +22,7 @@ const contenedorAtaques = document.getElementById('contenedor-ataques')
 const sectionVerMapa = document.getElementById('ver-mapa')
 const mapa = document.getElementById('mapa')
 
+let jugadorId = null
 
 let inputGorila 
 let inputLeopardo 
@@ -53,22 +54,33 @@ let intervalo
 
 let mapaBackground = new Image()
 mapaBackground.src = './assets/mokemap.png'
+let alturaQueBuscamos 
+let anchoDelMapa = window.innerWidth - 20
+
+alturaQueBuscamos = anchoDelMapa*600/800
+
+mapa.width = anchoDelMapa
+mapa.height = alturaQueBuscamos
+
+const anchoMaximoDelMapa = 350
+
+if(anchoDelMapa > anchoMaximoDelMapa)
+{
+    anchoDelMapa = anchoMaximoDelMapa - 20
+}
 
 class Mokepon {
-    constructor(nombre, foto, vida, fotoMapa, x=10, y=10){
+    constructor(nombre, foto, vida, fotoMapa){
         this.nombre = nombre
         this.foto = foto
         this.vida = vida
         this.ataques = []
-
-        this.x = x
-        this.y = y
         this.ancho = 50
         this.alto = 70
-
+        this.x = aleatorio(0, mapa.width - this.ancho)
+        this.y = aleatorio(0 , mapa.height - this.alto)
         this.mapaFoto = new Image()
         this.mapaFoto.src = fotoMapa
-
         this.velocidadX = 0
         this.velocidadY = 0
     }
@@ -87,14 +99,9 @@ let gorila = new Mokepon("Gorila", "./assets/gorila.png", 5, './assets/gorila.pn
 let leopardo = new Mokepon("Leopardo", "./assets/leopardo.png", 5, "./assets/leopardo.png")
 let zorro = new Mokepon("Zorro", "./assets/zorro.png", 5, "./assets/zorro.png")
 
-
-let gorilaEnemigo= new Mokepon("Gorila", "./assets/gorila.png", 5, './assets/gorila.png', 80,120)
-let leopardoEnemigo = new Mokepon("Leopardo", "./assets/leopardo.png", 5, "./assets/leopardo.png", 150, 95)
-let zorroEnemigo = new Mokepon("Zorro", "./assets/zorro.png", 5, "./assets/zorro.png", 250, 150)
-
-
-
-
+let gorilaEnemigo= new Mokepon("Gorila", "./assets/gorila.png", 5, './assets/gorila.png')
+let leopardoEnemigo = new Mokepon("Leopardo", "./assets/leopardo.png", 5, "./assets/leopardo.png")
+let zorroEnemigo = new Mokepon("Zorro", "./assets/zorro.png", 5, "./assets/zorro.png")
 
 gorila.ataques.push(
     {nombre: "tierra", id: "boton-tierra"},
@@ -177,6 +184,27 @@ function iniciarJuego()
     //@param: funcióv+n, función a ejecutar al hacer click en el botón, tipo de acción a escuchar del botón
     botonMascotaJugador.addEventListener('click', seleccionarMascota)
     botonReiniciar.addEventListener('click', reiniciar)
+
+    unirseAlJuego()
+}
+
+//Funcion que une la applicacion con el servidor
+function unirseAlJuego()
+{
+    fetch("http://localhost:8080/unirse")
+        .then(function (res) {
+            console.log(res)
+            if(res.ok)
+            {
+                res.text()
+                    .then(function(respuesta){
+                        jugadorId = respuesta
+                        console.log(respuesta)
+                    })
+            }
+        })
+
+
 }
 
 function seleccionarMascota()
@@ -204,8 +232,23 @@ function seleccionarMascota()
     {
         alert('Debe seleccionar una mascota')
     }
-    iniciarMapa()
 
+    seleccionarMokepon(mascotaJugador)
+
+    iniciarMapa()
+}
+
+function seleccionarMokepon(mascotaJugador)
+{
+    fetch("http://localhost:8080/mokepon/" + jugadorId, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            mokepon: mascotaJugador
+        })
+    })
 }
 
 function seleccionarMascotaEnemigo(enemigo)
@@ -378,18 +421,43 @@ function pintarCanvas()
     )
 
     mascotaJugadorObjeto.pintarMokepon()
+
     gorilaEnemigo.pintarMokepon()
     leopardoEnemigo.pintarMokepon()
     zorroEnemigo.pintarMokepon()
 
     if(mascotaJugadorObjeto.velocidadX !== 0 || mascotaJugadorObjeto.y !== 0)
     {
+        enviarPosicion(mascotaJugadorObjeto.x, mascotaJugadorObjeto.y)
+
         revisarColision(gorilaEnemigo)
         revisarColision(leopardoEnemigo)
         revisarColision(zorroEnemigo)
-    }
+    }   
+}
 
-    
+function enviarPosicion(x,y)
+{
+    fetch(`http://localhost:8080/mokepon/${jugadorId}/posicion`, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            x:x, // se puede escribir solo x dado que el valor y la llaven se llaman igual x:x, lo hago para y
+            y
+        })
+    })
+        .then(function(res)
+        {
+            if(res.ok)
+            {
+                res.json()
+                    .then(function({enemigos}){
+                        console.log(enemigos)
+                    })
+            }
+        })
 }
 
 function moverDerecha()
@@ -445,9 +513,6 @@ function sePresionoUnaTecla(event)
 
 function iniciarMapa()
 { 
-    mapa.width = 320
-    mapa.height = 240
-
     mascotaJugadorObjeto = obtenerObjetoMascota()
 
     intervalo = setInterval(pintarCanvas, 50)
